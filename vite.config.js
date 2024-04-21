244b3e5b8d72e28a92c833f10ff2d0f73da4f1f2
xyz2304 <tagojessem@outlook.fr>
2024-04-21 14:38:04 +0200

    /questionnaire-deploy/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "url";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "questionnaire-deploy",
+  base: "/questionnaire-deploy/",
   plugins: [vue()],
   resolve: {
     alias: [
